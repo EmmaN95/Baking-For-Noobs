897ee496a5dced2aa6818bcf6226ce82fe09ca5c
Emma <60002871+EmmaN95@users.noreply.github.com>
2020-07-30 11:14:10 +0100

    Add files via upload

--- recipes.php
+++ recipes.php
@@ -13,28 +13,35 @@ include_once('header.php');
     <p style="text-decoration: underline;">More great recipes to try!</p>
     
     <br>
-              <img src="Marssquare.jpg" alt="Mars" width="180" height="180" class="">
-              <p class="curly-font">Mars Bar Squares</p>
+              <img src="Marssquare.jpg" alt="Mars" width="180" height="180" class="" float: left;>
+              <br>
+              <p class='t'>Mars Bar Squares</p>
               <br>
          
-              <img src="peanutbutterkrispies.jpg" alt="peanutkrispie" width="180" height="180" class="">
-              <p class="curly-font">Peanut Butter Krispies</p>
+              <img src="peanutbutterkrispies.jpg" alt="peanutkrispie" width="180" height="180" class="" float: left;>
+              <br>
+              <p class='t'>Peanut Butter Krispies</p>
               <br>
         
               <img src="tiffin.jpg" alt="tiffin" width="180" height="180" class="">
-              <p class="curly-font">Tiffin</p>
+              <br>
+              <p class='t'>Tiffin</p>
               <br>
          
               <img src="blondies.jpg" alt="Blondies" width="180" height="180" class="">
-              <p class="curly-font">Blondies</p>
+              <br>
+              <p class='t'>Blondies</p>
               <br>
       
               <img src="oreobrownies.jpg" alt="oreobrownies" width="180" height="180" class="">
-              <p class="curly-font">Oreo Brownies</p>
+              <br>
+              <p class='t'>Oreo Brownies</p>
               <br>
         
               <img src="peanutbrownies.jpg" alt="peanutbrownies" width="180" height="180" class="">
-              <p class="curly-font">Peanut Butter Brownies</p>
+              <br>
+              <p class='t'>Peanut Butter Brownies</p>
+              <br>
           
   </div>
 
